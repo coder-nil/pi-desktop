287e739739a2ae0f0bc0c3d6b7c9e478e52b93b3
陈小尼 <chenxiaoni@tom.com>
2026-08-23 01:11:38 +0800

    chore(build): 更新前端构建文件
    - 更新页面组件的 JavaScript 文件哈希值
    - 更新布局组件的 JavaScript 文件哈希值
    - 更新构建 ID 以反映新的构建版本
    - 保持所有功能和样式配置不变

---
+++ src-tauri/frontend/_next/static/chunks/next/dist/client/components/builtin/global-error-bec4ffa5f6036d1d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9337],{62808:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,27123,23))}},e=>{e.O(0,[8441,3794,7358],()=>e(e.s=62808)),_N_E=e.O()}]);
